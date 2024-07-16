8c66e151d1f8ede75ab9abc51c919220f37560da
HengkerKucing <140718591+HengkerKucing@users.noreply.github.com>
2024-07-16 15:30:34 +0700

    (17) pending project

---
+++ api/models/gig.model.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const GigSchema = new Schema({
+    
+},{
+    timestamps:true
+}
+);
+
+export default mongoose.model("Gig", GigSchema)
--- api/server.js
+++ api/server.js
@@ -1,7 +1,23 @@
 import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
 
 const app = express()
+dotenv.config()
+mongoose.set('strictQuery', true)
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+    console.log("connected to mangoDB!")  
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
 
 app.listen(8800, () => {
+    connect()
     console.log("backend server is running!")
 })
