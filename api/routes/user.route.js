cf075ed118e5084c0a153e8ce585687baff3092c
HengkerKucing <140718591+HengkerKucing@users.noreply.github.com>
2024-07-19 18:36:46 +0700

    banyak library baru dan route, model, controller untuk login, register

---
+++ api/routes/user.route.js
@@ -0,0 +1,8 @@
+import express from "express"
+
+const router = express.Router()
+
+router.get("/register", )
+router.get("/login", )
+
+export default router
--- api/server.js
+++ api/server.js
@@ -1,6 +1,13 @@
 import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
+import userRoute from "./routes/user.route.js"
+import gigRoute from "./routes/gig.route.js"
+import orderRoute from "./routes/order.route.js"
+import conversationRoute from "./routes/conversation.route.js"
+import messageRoute from "./routes/message.route.js"
+import reviewRoute from "./routes/review.route.js"
+import authRoute from "./routes/auth.route.js"
 
 const app = express()
 dotenv.config()
@@ -14,6 +21,16 @@ const connect = async () => {
         console.log(error);
     }
 }
+
+app.use(express.json())
+
+app.use("/api/auth", authRoute)
+app.use("/api/users", userRoute)
+app.use("/api/gigs", gigRoute)
+app.use("/api/orders", orderRoute)
+app.use("/api/conversations", conversationRoute)
+app.use("/api/messages", messageRoute)
+app.use("/api/reviews", reviewRoute)
 
 
 
