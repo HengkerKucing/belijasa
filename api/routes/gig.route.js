22cdbbe5a511b89984e3c62604087c472429b9f1
HengkerKucing <140718591+HengkerKucing@users.noreply.github.com>
2024-08-13 08:22:05 +0700

    gig CRUD & req.query

--- api/routes/gig.route.js
+++ api/routes/gig.route.js
@@ -1,7 +1,17 @@
 import express from "express"
+import { verifyToken } from './../middleware/jwt.js';
+import {
+    createGig,
+    deleteGig,
+    getGig,
+    getGigs
+} from "../controllers/gig.controller.js"
 
 const router = express.Router()
 
-router.get("/test", )
+router.post("/", verifyToken, createGig )
+router.delete("/:id", verifyToken, deleteGig )
+router.get("/single/:id", verifyToken, getGig )
+router.get("/", verifyToken, getGigs )
 
 export default router
